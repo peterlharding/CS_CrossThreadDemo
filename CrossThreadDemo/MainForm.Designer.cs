8c6c5fb923604b674443a780673b6a05fa9c66c4
Peter Harding <plh@magusco.com>
2017-10-24 12:35:05 +1100

    Initial commit

---
+++ CrossThreadDemo/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿using System.Windows.Forms;
+
+namespace CrossThreadDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_Demo = new System.Windows.Forms.TextBox();
+            this.Btn_SetText_Unsafe = new System.Windows.Forms.Button();
+            this.Btn_SetText_Safe = new System.Windows.Forms.Button();
+            this.Btn_SetTextInBackgroundWorker = new System.Windows.Forms.Button();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // Txt_Demo
+            // 
+            this.Txt_Demo.Location = new System.Drawing.Point(12, 12);
+            this.Txt_Demo.Name = "Txt_Demo";
+            this.Txt_Demo.Size = new System.Drawing.Size(408, 20);
+            this.Txt_Demo.TabIndex = 0;
+            // 
+            // Btn_SetText_Unsafe
+            // 
+            this.Btn_SetText_Unsafe.Location = new System.Drawing.Point(15, 55);
+            this.Btn_SetText_Unsafe.Name = "Btn_SetText_Unsafe";
+            this.Btn_SetText_Unsafe.Size = new System.Drawing.Size(97, 23);
+            this.Btn_SetText_Unsafe.TabIndex = 1;
+            this.Btn_SetText_Unsafe.Text = "Unsafe Set Text";
+            this.Btn_SetText_Unsafe.Click += new System.EventHandler(this.Btn_SetText_Unsafe_Click);
+            // 
+            // Btn_SetText_Safe
+            // 
+            this.Btn_SetText_Safe.Location = new System.Drawing.Point(118, 55);
+            this.Btn_SetText_Safe.Name = "Btn_SetText_Safe";
+            this.Btn_SetText_Safe.Size = new System.Drawing.Size(96, 23);
+            this.Btn_SetText_Safe.TabIndex = 2;
+            this.Btn_SetText_Safe.Text = "Safe Set Text";
+            this.Btn_SetText_Safe.Click += new System.EventHandler(this.Btn_SetText_Safe_Click);
+            // 
+            // Btn_SetTextInBackgroundWorker
+            // 
+            this.Btn_SetTextInBackgroundWorker.Location = new System.Drawing.Point(220, 55);
+            this.Btn_SetTextInBackgroundWorker.Name = "Btn_SetTextInBackgroundWorker";
+            this.Btn_SetTextInBackgroundWorker.Size = new System.Drawing.Size(200, 23);
+            this.Btn_SetTextInBackgroundWorker.TabIndex = 3;
+            this.Btn_SetTextInBackgroundWorker.Text = "Set Text from Background Worker";
+            this.Btn_SetTextInBackgroundWorker.Click += new System.EventHandler(this.Btn_SetTextFromBackgroundWorker_Click);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(431, 96);
+            this.Controls.Add(this.Btn_SetTextInBackgroundWorker);
+            this.Controls.Add(this.Btn_SetText_Safe);
+            this.Controls.Add(this.Btn_SetText_Unsafe);
+            this.Controls.Add(this.Txt_Demo);
+            this.Name = "MainForm";
+            this.Text = "Cross Thred Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox Txt_Demo;
+        private Button Btn_SetText_Unsafe;
+        private Button Btn_SetText_Safe;
+        private Button Btn_SetTextInBackgroundWorker;
+
+
+    }
+}
+
---
+++ CrossThreadDemo/MainForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CrossThreadDemo
+{
+    public partial class MainForm : Form
+    {
+
+        // This delegate enables asynchronous calls for setting the text property on a TextBox control.
+
+        private delegate void SetTextCallback(string text);
+
+        // This thread is used to demonstrate both thread-safe and unsafe ways to call a Windows Forms control.
+
+        private Thread demoThread = null;
+
+        // This BackgroundWorker is used to demonstrate the preferred way of performing asynchronous operations.
+
+        private BackgroundWorker backgroundWorker;
+
+        //-----------------------------------------------------------------------------------------
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+        } // MainForm
+
+        //-----------------------------------------------------------------------------------------
+
+        // This event handler creates a thread that calls a Windows Forms control in an unsafe way.
+
+        private void Btn_SetText_Unsafe_Click(object sender, EventArgs e)
+        {
+            demoThread = new Thread(ThreadProcUnsafe);
+
+            demoThread.Start();
+
+        } // Btn_SetText_Unsafe_Click
+
+        //-----------------------------------------------------------------------------------------
+        // This method is executed on the worker thread and makes an unsafe call on the TextBox control.
+
+        private void ThreadProcUnsafe()
+        {
+            Txt_Demo.Text = @"This text was set unsafely.";
+
+        } // ThreadProcUnsafe
+
+        //-----------------------------------------------------------------------------------------
+        // This event handler creates a thread that calls a Windows Forms control in a thread-safe way.
+
+        private void Btn_SetText_Safe_Click(object sender, EventArgs e)
+        {
+            demoThread = new Thread(ThreadProcSafe);
+
+            demoThread.Start();
+
+        } // Btn_SetText_Safe_Click
+
+        // This method is executed on the worker thread and makes a thread-safe call on the TextBox control.
+
+        private void ThreadProcSafe()
+        {
+            SetText(@"This text was set safely.");
+
+        } // ThreadProcSafe
+
+        //-----------------------------------------------------------------------------------------
+        // This method demonstrates a pattern for making thread-safe calls on a Windows Forms control.  
+        // 
+        // If the calling thread is different from the thread that created the TextBox control,
+        // this method creates a SetTextCallback and calls itself asynchronously using the 
+        // Invoke method. 
+        // 
+        // If the calling thread is the same as the thread that created the TextBox control,
+        // the Text property is set directly.  
+
+        public void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the calling thread to the
+            // thread ID of the creating thread. If these threads are different, it returns true. 
+
+            if (Txt_Demo.InvokeRequired)
+            {
+                SetTextCallback d = SetText;
+                Invoke(d, text);
+            }
+            else
+            {
+                Txt_Demo.Text = text;
+            }
+        }
+
+        //-----------------------------------------------------------------------------------------
+        // This event handler starts the form's BackgroundWorker by calling RunWorkerAsync. 
+        // 
+        // The Text property of the TextBox control is set when the BackgroundWorker raises
+        // the RunWorkerCompleted event.
+
+        private void Btn_SetTextFromBackgroundWorker_Click(object sender, EventArgs e)
+        {
+            backgroundWorker.RunWorkerAsync();
+
+        } // Btn_SetTextFromBackgroundWorker_Click
+
+        //-----------------------------------------------------------------------------------------
+        // This event handler sets the Text property of the TextBox control. It is called on the
+        // thread that created the TextBox control, so the call is thread-safe. 
+        // 
+        // BackgroundWorker is the preferred way to perform asynchronous operations. 
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Txt_Demo.Text = @"This text was set safely by BackgroundWorker.";
+
+        } // backgroundWorker_RunWorkerCompleted
+
+        //-----------------------------------------------------------------------------------------
+
+    }
+}
